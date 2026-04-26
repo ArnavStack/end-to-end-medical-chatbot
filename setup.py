d04e5a64a6af35d0327d2acdeefd8a5e9a678641
apnacollege <arnavcode12@gmail.com>
2026-04-26 20:07:37 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Arnav Thakur',
+    author_email= 'arnavcode12@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+    
+)
